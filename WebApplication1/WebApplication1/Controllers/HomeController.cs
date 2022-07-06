cbe504da36fc84d0eede7b5b20d48767ad7e0471
Sabir M. Guliyev <sabirmg@code.local>
2022-07-06 13:14:49 +0400

    Task_Added

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            List<Service> services = _context.Services.ToList();
+            return View(services);
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id==null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Service service = _context.Services.FirstOrDefault(x => x.Id == id);
+            return View(service);
+            
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Article { get; set; }
+        public string Icon { get; set; }
+    }
+}
